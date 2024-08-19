1b108fb0a97ae9dafd0d4a94310da6e6eede71ef
용성 <ehfrhfo9494@naver.com>
2024-08-19 14:37:59 +0900

    MemberRepository

---
+++ src/main/java/Climbing/Record/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package Climbing.Record.repository;
+
+import Climbing.Record.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+}
