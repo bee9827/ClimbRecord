964868fd24822fba407c99c6dda04ffbdd6035a8
용성 <ehfrhfo9494@naver.com>
2024-08-19 14:38:20 +0900

    RecordRepository 생성

---
+++ src/main/java/Climbing/Record/repository/RecordRepository.java
@@ -0,0 +1,8 @@
+package Climbing.Record.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record,Integer> {
+}
